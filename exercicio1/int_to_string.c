430dc5a21895571375f101b47e3682332722de20
TiagoNevoa <tiagonevoa@gmail.com>
2022-10-25 20:48:51 +0100

    Update int_to_string.c
    Ex1 update

--- exercicio1/int_to_string.c
+++ exercicio1/int_to_string.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 size_t int_to_string(int value, int base, char buffer[], size_t buffer_size){
 
+	// problably we need count the number of digits from value first
+
+	char base_caracter [] = {'0','1','2','3','4','5','6','8','9',
+							'A','B','C','D','E','F'};
 	int v = value;
 
 	while (v > 0){
-		printf("n: %d \n",(v % base));
+		printf("n: %c \n",base_caracter[(v % base)]);
 		v = v/base;
 	}
 
@@ -15,6 +19,6 @@ size_t int_to_string(int value, int base, char buffer[], size_t buffer_size){
 int main (void){
 	size_t buffer_size = 100;
 	char buffer [buffer_size];
-	int_to_string(17689,2,buffer,buffer_size);
+	int_to_string(627,10,buffer,buffer_size);
 	return 0;
 }
